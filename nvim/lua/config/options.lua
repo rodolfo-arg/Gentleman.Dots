de4c26c5c944bee37526adc60067c526531c63e4
Rodolfo <rodolfo.arguello@rode.com>
2025-09-04 09:00:42 +1000

    remove save formatting, fix terminal window not focusing

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,16 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "cpp",
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})
+
+vim.api.nvim_create_autocmd("TermOpen", {
+  callback = function()
+    vim.cmd("startInsert")
+  end,
+})
