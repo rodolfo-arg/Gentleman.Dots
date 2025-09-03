e6f629ba9acc4b1c7438b63ad604280dc4f770d8
Rodolfo <rodolfo.arguello@rode.com>
2025-09-04 09:13:11 +1000

    attempt to fix terminal windows switching capabilities

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -10,7 +10,21 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 vim.api.nvim_create_autocmd("TermOpen", {
+  pattern = "term://*",
   callback = function()
-    vim.cmd("startInsert")
+    -- Always start in insert mode
+    vim.cmd("startinsert")
+
+    -- Make sure <C-w> works in terminal normal mode
+    vim.keymap.set("n", "<C-w>h", "<C-w>h", { buffer = true })
+    vim.keymap.set("n", "<C-w>j", "<C-w>j", { buffer = true })
+    vim.keymap.set("n", "<C-w>k", "<C-w>k", { buffer = true })
+    vim.keymap.set("n", "<C-w>l", "<C-w>l", { buffer = true })
+
+    -- Seamless movement from terminal insert mode
+    vim.keymap.set("t", "<C-h>", [[<C-\><C-n><C-w>h]], { buffer = true })
+    vim.keymap.set("t", "<C-j>", [[<C-\><C-n><C-w>j]], { buffer = true })
+    vim.keymap.set("t", "<C-k>", [[<C-\><C-n><C-w>k]], { buffer = true })
+    vim.keymap.set("t", "<C-l>", [[<C-\><C-n><C-w>l]], { buffer = true })
   end,
 })
