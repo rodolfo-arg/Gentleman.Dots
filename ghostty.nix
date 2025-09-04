f3cf38b59e7edc371ec62237fcfcbb6f0cf330d7
Rodolfo <rodolfo.arguello@rode.com>
2025-09-04 15:19:58 +1000

    test

--- ghostty.nix
+++ ghostty.nix
@@ -8,7 +8,7 @@
     if [ ! -d "$HOME/.config/ghostty" ]; then
       mkdir -p "$HOME/.config/ghostty"
     fi
-
+    
     cp -r ${toString ./ghostty}/* "$HOME/.config/ghostty/"
     chmod -R u+w "$HOME/.config/ghostty"
   '';
