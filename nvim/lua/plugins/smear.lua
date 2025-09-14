8584dbaf44ab423b941b5d423d075be68639557f
Rodolfo <rodolfo.arguello@rode.com>
2025-09-15 00:41:38 +1000

    nvim: scroll smoothing

--- nvim/lua/plugins/smear.lua
+++ nvim/lua/plugins/smear.lua
@@ -1,9 +1,8 @@
 return {
   "sphamba/smear-cursor.nvim",
   opts = {
-    -- Make the trail more elastic/springy vs. rigid
-    stiffness = 0.7,      -- lower = softer, higher = stiffer
-    -- Update more frequently for smoother motion
-    interval = 8,         -- ms between updates
+    -- Faster, smoother smear
+    stiffness = 0.85,     -- a bit snappier while keeping fluidity
+    interval = 5,         -- more frequent updates (ms)
   },
 }
