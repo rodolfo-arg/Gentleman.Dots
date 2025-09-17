59a0d3ab900bb0e67f723ef38c073c9d437bab14
Rodolfo <rodolfo.arguello@rode.com>
2025-09-17 15:44:15 +1000

    feat: linux support

--- neovide.nix
+++ neovide.nix
@@ -4,19 +4,9 @@ let
   pkgSet = if unstablePkgs == null then pkgs else unstablePkgs;
 in
 {
-  # Install Neovide on macOS and Linux
-  home.packages = lib.mkIf (pkgs.stdenv.isDarwin || pkgs.stdenv.isLinux) [ pkgSet.neovide ];
-
-  # Neovide config (XDG path)
-  home.file.".config/neovide/config.toml" = lib.mkIf (pkgs.stdenv.isDarwin || pkgs.stdenv.isLinux) {
-    text = ''
-      frame = "none"
-    '';
-    force = true;
-  };
-
-  # Provide a cross-shell launcher "svim" without touching deprecated zsh.initExtra
+  # Install Neovide and provide a portable "svim" launcher on macOS and Linux
   home.packages = lib.mkIf (pkgs.stdenv.isDarwin || pkgs.stdenv.isLinux) [
+    pkgSet.neovide
     (pkgs.writeShellScriptBin "svim" ''
       #!/usr/bin/env bash
       if [ $# -eq 0 ]; then
@@ -26,4 +16,12 @@ in
       fi
     '')
   ];
+
+  # Neovide config (XDG path)
+  home.file.".config/neovide/config.toml" = lib.mkIf (pkgs.stdenv.isDarwin || pkgs.stdenv.isLinux) {
+    text = ''
+      frame = "none"
+    '';
+    force = true;
+  };
 }
