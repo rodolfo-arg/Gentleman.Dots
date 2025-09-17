afb11208fc6c6f5dd709be7c816567da3f799f4b
Rodolfo <rodolfo.arguello@rode.com>
2025-09-17 16:19:32 +1000

    feat: linux support

--- neovide.nix
+++ neovide.nix
@@ -5,34 +5,36 @@ let
 in
 {
   # Install Neovide and provide a portable "svim" launcher on macOS and Linux
-  home.packages = lib.mkIf (pkgs.stdenv.isDarwin || pkgs.stdenv.isLinux)
-    ([
-      pkgSet.neovide
-      (pkgs.writeShellScriptBin "svim" ''
-        #!/usr/bin/env bash
-        # On Linux, prefer software GL and X11 for compatibility
-        if [ "$(uname -s)" = "Linux" ]; then
-          export LIBGL_ALWAYS_SOFTWARE="''${LIBGL_ALWAYS_SOFTWARE:-1}"
-          export MESA_LOADER_DRIVER_OVERRIDE="''${MESA_LOADER_DRIVER_OVERRIDE:-llvmpipe}"
-          if [ -n "''${DISPLAY-}" ]; then
-            export WINIT_UNIX_BACKEND="''${WINIT_UNIX_BACKEND:-x11}"
+  home.packages =
+    (if pkgs.stdenv.isDarwin || pkgs.stdenv.isLinux then
+      ([
+        pkgSet.neovide
+        (pkgs.writeShellScriptBin "svim" ''
+          #!/usr/bin/env bash
+          # On Linux, prefer software GL and X11 for compatibility
+          if [ "$(uname -s)" = "Linux" ]; then
+            export LIBGL_ALWAYS_SOFTWARE="''${LIBGL_ALWAYS_SOFTWARE:-1}"
+            export MESA_LOADER_DRIVER_OVERRIDE="''${MESA_LOADER_DRIVER_OVERRIDE:-llvmpipe}"
+            if [ -n "''${DISPLAY-}" ]; then
+              export WINIT_UNIX_BACKEND="''${WINIT_UNIX_BACKEND:-x11}"
+            fi
           fi
-        fi
-        if [ $# -eq 0 ]; then
-          exec ${pkgSet.neovide}/bin/neovide --fork
-        else
-          exec ${pkgSet.neovide}/bin/neovide --fork "$@"
-        fi
-      '')
-    ] ++ lib.optionals pkgs.stdenv.isLinux [
-      (pkgs.writeShellScriptBin "neovide-soft" ''
-        #!/usr/bin/env bash
-        export LIBGL_ALWAYS_SOFTWARE=1
-        export MESA_LOADER_DRIVER_OVERRIDE=llvmpipe
-        export WINIT_UNIX_BACKEND=x11
-        exec ${pkgSet.neovide}/bin/neovide "$@"
-      '')
-    ]);
+          if [ $# -eq 0 ]; then
+            exec ${pkgSet.neovide}/bin/neovide --fork
+          else
+            exec ${pkgSet.neovide}/bin/neovide --fork "$@"
+          fi
+        '')
+      ] ++ (if pkgs.stdenv.isLinux then [
+        (pkgs.writeShellScriptBin "neovide-soft" ''
+          #!/usr/bin/env bash
+          export LIBGL_ALWAYS_SOFTWARE=1
+          export MESA_LOADER_DRIVER_OVERRIDE=llvmpipe
+          export WINIT_UNIX_BACKEND=x11
+          exec ${pkgSet.neovide}/bin/neovide "$@"
+        '')
+      ] else []))
+    else [];
 
   # Neovide config (XDG path)
   home.file.".config/neovide/config.toml" = lib.mkIf (pkgs.stdenv.isDarwin || pkgs.stdenv.isLinux) {
